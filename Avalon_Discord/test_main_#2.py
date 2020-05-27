1c2330d49b40ec190b8da1c4fe6641b46396de9c
unknown <Liciliy@github.com>
2020-05-28 00:59:44 +0300

    Test main changes.

--- Avalon_Discord/test_main_#2.py
+++ Avalon_Discord/test_main_#2.py
@@ -34,6 +34,48 @@ def paths():
 
     print('=======================================================\n')
 
+class GeometryPoint:
+    x_coord = None
+    y_coord = None
+
+    def __init__(self, x, y):
+        self.x_coord = x
+        self.y_coord = x
+
+    def __truediv__(self, dividor_int):
+
+        #if (type(dividor_int) == type(int)):
+
+        return GeometryPoint(x = int(self.x_coord/dividor_int),
+                             y = int(self.y_coord/dividor_int))
+
+        #if (type(dividor_int) == type(GeometryPoint)):
+               
+
+    def __sub__(self, other_point):
+
+        return GeometryPoint(x = self.x_coord - other_point.x_coord,
+                             y = self.y_coord - other_point.y_coord) 
+
+    def __str__(self):
+        return '(' + str(self.x_coord) + ', ' + str(self.y_coord) + ')'
+
+    def to_tuple(self):
+        return (self.x_coord, self.y_coord)
+
+
 
 if __name__ == "__main__":
-    paths()
+    my_point   = GeometryPoint(80, 80)
+
+    print ('my_point ', my_point)
+    this_point = GeometryPoint(30, 30)
+
+    print ('this_point ', this_point)
+
+    new_point = my_point - (this_point / 2)
+
+    print('new_point ', new_point)
+
+    print ('new_point tuple', new_point.to_tuple())
+    print (type (new_point.to_tuple()))
